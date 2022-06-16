c3fdc7d79ba5200b405e2ff8a41da1ebbb80a39b
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-06-16 18:45:42 +0200

    redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, incrementByNumber } from './actions';
 import './App.css';
 
 function App() {
-  return <h1>
-    REDUX
-  </h1>;
+  const counter = useSelector(state => state.counter);
+  const isLoggedIn = useSelector(state => state.isLoggedIn);
+  const dispatch = useDispatch();
+
+  return <div className='counter-wrapper'>
+    <div>
+    <h1>{counter}</h1>
+    <button onClick={() => dispatch(decrement())}>-</button>
+    <button onClick={() => dispatch(increment())}>+</button>
+
+    {/* <button onClick={() => dispatch(incrementByNumber(10))}>+</button> */}
+
+    {isLoggedIn && <h3>SECRET DATA</h3>}
+    </div>
+  </div>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./App";
+import allReducers from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
